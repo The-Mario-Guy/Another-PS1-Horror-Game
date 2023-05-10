d58158a670c7af3810f37d53ced98bfd67963e88
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-05-10 11:04:17 -0700

    Yes

---
+++ Another PS1 Horror Game/Assets/Scenes/Cutscenes/Clue3.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Clue3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(22.5f);
+        SceneManager.LoadScene(2);
+    }
+}
